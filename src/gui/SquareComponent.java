34763e7b69a9c8b226bac75a2660a1d376913b40
Sebastien Duc <sebastien.sduc@gmail.com>
2015-12-13 02:05:22 +0000

    Fix the Sqaure highlighting on select

--- src/gui/SquareComponent.java
+++ src/gui/SquareComponent.java
@@ -15,27 +15,30 @@ public class SquareComponent extends JPanel {
 	private PieceComponent piece;
 	
 	private Position position;
+
+	private java.awt.Color color;
 	
 	public SquareComponent(Position p, Color c) {
 		piece = null;
 		position = p;
-		
+
 		init(c);
 	}
 	
 	private void init(Color c) {
 		switch (c) {
 		case BLACK:
-			this.setBackground(java.awt.Color.GRAY);
+			this.color = java.awt.Color.GRAY;
 			break;
 
 		case WHITE:
-			this.setBackground(java.awt.Color.WHITE);
+			this.color = java.awt.Color.WHITE;
 			break;
 			
 		default:
 			assert(false);
 		}
+		this.setBackground(this.color);
 	}
 	
 	public void setPiece(PieceComponent pc) {
@@ -61,9 +64,20 @@ public class SquareComponent extends JPanel {
 			piece.repaint();
 	}
 
+	private void updateColor(boolean on) {
+		if (on) {
+			this.setBackground(java.awt.Color.CYAN);
+		} else {
+			this.setBackground(this.color);
+		}
+	}
+
 	public void highlight(boolean on) {
 		if (piece != null)
 			piece.highlight(on);
+		else
+			updateColor(on);
+		this.repaint();
 		//might want to repaint maybe TODO
 	}
 
